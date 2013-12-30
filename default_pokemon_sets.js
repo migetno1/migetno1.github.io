0534e9193365640ca67e7f9ac0df598191082736
migetno1 <migetno1@gmail.com>
2013-12-30 12:23:59 +1100

    default set changes

--- default_pokemon_sets.js
+++ default_pokemon_sets.js
@@ -46,12 +46,12 @@ var DEFAULT_POKEMON_SETS = {
       {
          name : 'Specially Defensive (generic)',
          nature : 'Calm',
-         ev : [ 252, 0, 252, 0, 4, 0 ],
+         ev : [ 252, 0, 4, 0, 252, 0 ],
       },
       {
          name : 'Bulky (generic)',
          nature : 'Hardy',
-         ev : [ 252, 0, 252, 0, 4, 0 ],
+         ev : [ 252, 0, 0, 0, 0, 0 ],
       },
    ],
 };
